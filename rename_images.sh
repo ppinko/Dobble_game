0cb7f467f9a1d6c9401f98da145444a41ceb9165
legpol <p.pinkowicz@gmail.com>
2019-12-21 21:24:56 +0100

    Changing game pictures to cartooons

--- rename_images.sh
+++ rename_images.sh
@@ -2,7 +2,7 @@
 
 cd ~/all/python/projects/dobble/images/
 i=0
-for filename in ~/all/python/projects/dobble/images/*; do
+for filename in ~/all/python/projects/dobble/images_cartoon/*; do
 	new_name="img_$i.png"
 	let i=$i+1 
 	mv $filename $new_name
